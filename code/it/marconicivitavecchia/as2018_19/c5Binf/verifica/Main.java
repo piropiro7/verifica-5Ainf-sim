39538a847ec98df30b376892877c3edc6ec76b3d
Your Name <you@example.com>
2018-11-10 11:34:03 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
@@ -12,16 +12,45 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(600,500);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb = new StringBuilder("<html>");
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<h1><b>Formula 1 Passion</b></h1>");
+		sb.append("<div style='' >");
+		sb.append("<div style='display:table-row>'");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("Pilota");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("Punteggio");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("HAMILTON L.");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("358");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("VETTEL S.");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("294");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("RAIKKONEN K.");
+		sb.append("</div>");
+		sb.append("<div style='display:table-row-cell'>");
+		sb.append("236");
+		sb.append("</div>");
 		
+        sb.append("</div>");
+		sb.append("</html>");
 		// TODO Convertire lo StringBuilder in String
-		
+		String str = sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(str));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
